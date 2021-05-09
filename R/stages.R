0db49f2bd4a9b24875aa27ee3e86e277adf5edb0
Ramiro Magno <ramiro.magno@gmail.com>
2021-05-09 15:22:20 +0100

    Add `stages` dataset

---
+++ R/stages.R
@@ -0,0 +1,18 @@
+#' Study stages
+#'
+#' A dataset containing the various study stages assigned to samples in the PGS Catalog.
+#'
+#' @format A data frame with 5 stages (rows) and 4 columns:
+#' \describe{
+#'   \item{stage}{Study stage.}
+#'   \item{symbol}{One-letter symbol for the stage, or a comma separated combination thereof.}
+#'   \item{name}{Stage name.}
+#'   \item{definition}{Stage description.}
+#' }
+#'
+#' @examples
+#' stages
+#'
+#' @source \url{https://www.pgscatalog.org/docs/ancestry}
+#'
+"stages"
