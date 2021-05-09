7cbea288357ed287ca221977766d9e0bd83483f1
Ramiro Magno <ramiro.magno@gmail.com>
2021-05-09 01:13:20 +0100

    Updated is_paginated() test

--- tests/testthat/test-is_paginated.R
+++ tests/testthat/test-is_paginated.R
@@ -1,13 +1,14 @@
 test_that("is not paginated", {
   expect_false(is_paginated(''))
-  expect_false(is_paginated('{\n  \"id\": \"PGP000001\",\n '))
+  expect_false(is_paginated('{\n  \"id\": \"PGP000001\"\n }'))
 })
 
 test_that("is paginated", {
   txt <- '{
   "count": 6,
   "next": null,
-  "previous": null,
+  "previous": null
+  }
 '
   expect_true(is_paginated(txt))
 })
